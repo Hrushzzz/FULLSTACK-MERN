983a26a9dcba1155fb85e237c0919ba1ad545fac
SpiderMan <hrushikesh.tk@gmail.com>
2024-10-22 01:48:07 +0530

    private methods

--- Ch02 - Javascript/Day10 - OOPS 2 - Classes and Constructors/classes.js
+++ Ch02 - Javascript/Day10 - OOPS 2 - Classes and Constructors/classes.js
@@ -1,15 +1,27 @@
 // *** Creating a Class ***
 
 class Pizza {
+    static totalPizzasMade = 0;  //static value
+
+    #toppings;   //private variables
+    #size;
+    #crust;
+
     //constructor method
     constructor(toppings , size , crust) {
-        this.toppings = toppings;
-        this.size = size;
-        this.crust = crust;
+        this.#toppings = toppings;
+        this.#size = size;
+        this.#crust = crust;
+        //***Static methods belong to a class itself rather than to instances of the class***
+        Pizza.totalPizzasMade++;
     }
 
     describe() {
-        console.log(`This pizza has topping as ${this.toppings} and is of ${this.size} size and its crust is ${this.crust}`);
+        console.log(`This Pizza has ${this.#toppings} with ${this.#crust} crust and a ${this.#size} Size`);
+    }
+
+    static calculateNumberOfPizzas() {    //static method
+        console.log(`Total Pizzas Made : ${Pizza.totalPizzasMade}`);
     }
 }
 
@@ -30,4 +42,12 @@ class StuffedPizza extends Pizza{
 
 
 const Pizza2 = new StuffedPizza("butter" , "large" , "thick" , "cheese and garlic");
+const Pizza3 = new StuffedPizza("butter" , "small" , "thin" , "chilli");
 console.log(Pizza2);
+
+Pizza.calculateNumberOfPizzas();
+
+//Note :: To use static values, we create static methods.
+// To compute or modify static values , we create static methods.
+
+//we can call static property from a non-static function
