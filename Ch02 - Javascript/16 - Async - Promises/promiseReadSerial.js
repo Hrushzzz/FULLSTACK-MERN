e7d118104fe142c1d30b8fb8d569906e20e3936d
SpiderMan <hrushikesh.tk@gmail.com>
2024-11-12 01:49:14 +0530

    promises

--- Ch02 - Javascript/16 - Async - Promises/promiseReadSerial.js
+++ Ch02 - Javascript/16 - Async - Promises/promiseReadSerial.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const {error} = require("console");
+const { error } = require("console");
 
 // fs.readFile("f1.txt" , cb);
 
@@ -16,28 +16,27 @@ console.log("Before");
 let promiseReadFile1 = fs.promises.readFile("f1.txt");
 
 function cb1(data) {
-    console.log("This is my file data -> " + data);
+  console.log("This is my file data -> " + data);
 
-    let promiseReadFile2 = fs.promises.readFile("f2.txt");
-    return promiseReadFile2;
+  let promiseReadFile2 = fs.promises.readFile("f2.txt");
+  return promiseReadFile2;
 }
 
 function cb2(data) {
-    console.log("This is my file data -> " + data);
+  console.log("This is my file data -> " + data);
 
-    let promiseReadFile3 = fs.promises.readFile("f3.txt");
-    return promiseReadFile3;
+  let promiseReadFile3 = fs.promises.readFile("f3.txt");
+  return promiseReadFile3;
 }
 
 function cb3(data) {
-    console.log("This is my file data -> " + data);
-    }
-
+  console.log("This is my file data -> " + data);
+}
 
 promiseReadFile1
-.then(cb1)
-.then(cb2)
-.then(cb3)
-.catch(function(err) {
+  .then(cb1)
+  .then(cb2)
+  .then(cb3)
+  .catch(function (err) {
     console.log(err);
-});
+  });
