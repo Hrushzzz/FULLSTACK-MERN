f90102d817a3a656c8cc85f6aa694bb5bb44c6fa
SpiderMan <hrushikesh.tk@gmail.com>
2024-11-26 01:39:35 +0530

    polyfills of HOF

--- Ch02 - Javascript/14 - OOPS 5 - Polyfiils of Higher Order Methods/sampleQuestions.js
+++ Ch02 - Javascript/14 - OOPS 5 - Polyfiils of Higher Order Methods/sampleQuestions.js
@@ -1,99 +1,104 @@
-// Q1. Name initials
+// => Q1. Name initials
 
 // const input = "Messi Suarez Neymar";
 // output = MSN
  
-const input = "Messi Suarez Neymar";
+// const input = "Messi Suarez Neymar";
 
-const output = input.split(' ').map(function(word){
-    return word[0];
-}).join('');
+// const output = input.split(' ').map(function(word){
+//     return word[0];
+// }).join('');
 
-console.log(output)
+// console.log(output);
 
 //------------------------------------------------------------------------------------//
 
-//   Q2. Given an array of objects users, print user of particular age along with their frequency.
+// => Q2. Given an array of objects users, print user of particular age along with their frequency.
 
 //   const users=[
-//     {firstName:"john",lastName:"Biden",age:26},
-//     {firstName:"jimmy",lastName:"cob",age:75},
-//     {firstName:"sam",lastName:"lewis",age:50},
-//     {firstName:"Ronald",lastName:"Mathew",age:26}, 
-//     {firstName:"Sachin",lastName:"Tendulkar",age:50}, 
-//     {firstName:"Sachin",lastName:"Tendulkar",age:50},
+//     {firstName:"Lionel",lastName:"Messi",age:37},
+//     {firstName:"MS",lastName:"Dhoni",age:43},
+//     {firstName:"Prabhas",lastName:"Raju",age:43},
+//     {firstName:"Marc",lastName:"Marquez",age:28}, 
+//     {firstName:"Louis",lastName:"Hamilton",age:37}, 
+//     {firstName:"Travis",lastName:"Head",age:50},
 //   ];
 
 
-//   ouput ->
-//   26 - 2
-//   75 - 1
-//   50 - 2
+// // output: { '28': 1, '37': 2, '43': 2, '50': 1 }
 
 
-// const ans = users.reduce(function(acc , curr){
-//     if(acc[curr.age]){
-//         acc[curr.age]++
+// const ans = users.reduce(function(acc , curr) {
+//     if(acc[curr.age]) {
+//         acc[curr.age]++;
 //     }
-//     else{
-//         acc[curr.age] = 1
+//     else {
+//         acc[curr.age] = 1;
 //     }
 
 //     return acc
-// } , {})
+// } , {});
 
-
-// console.log(ans)
+// console.log(ans);
 
 //------------------------------------------------------------------------------------//
 
-// method Chaining
+// Method Chaining
 
-// // Q4 Print the names of students who scored more than 60
+// => Q3. Print the names of students who scored more than 60
 
 
-// let student =[
-//     {name:"Smith",rollNumber:31,marks:80},
-//     {name:"Jenny",rollNumber:15,marks:69},
-//     {name:"John",rollNumber:16,marks:35},
-//     {name:"Tiger",rollNumber:7,marks:55}
+// let student = [
+//     {name:"Dheeraj",rollNumber:31,marks:80},
+//     {name:"Kuldeep",rollNumber:15,marks:69},
+//     {name:"Rakesh",rollNumber:16,marks:35},
+//     {name:"Hrishi",rollNumber:7,marks:55}
 //    ];
 
 
-//    const results = student.filter(function(obj){
+//    const results = student.filter(function(obj) {
 //     return obj.marks > 60
 //    }).map(function(obj){
-//     return obj.name
-//    })
+//     return obj.name;
+//    });
 
-//    console.log(results)
+//    console.log(results);
 
 //------------------------------------------------------------------------------------//
 
-// Q1. Given an array of objects users, print fullname.
+// => Q4. Given an array of objects users, print fullname.
 
 
 // const users=[
-//     {firstName:"john",lastName:"Biden",age:26},
-//     {firstName:"jimmy",lastName:"cob",age:75},
-//     {firstName:"sam",lastName:"lewis",age:50},
-//     {firstName:"Ronald",lastName:"Mathew",age:26},  
+//     {firstName:"Dheeraj",lastName:"Kohli",age:26},
+//     {firstName:"Kuldeep",lastName:"Raina",age:75},
+//     {firstName:"Hrishi",lastName:"Messi",age:50},
+//     {firstName:"Rakesh",lastName:"Dhoni",age:26},  
 //   ];
 
 
-// Solution here ->
+// const results = users.map(function(obj) {
+//     return obj.firstName + " " + obj.lastName;
+// });
+
+// console.log(results);
 
 //------------------------------------------------------------------------------------//
 
-// // Q3.Print object having marks greater than 60 and rollNumber greater than 15.
+// => Q5.Print object having marks greater than 60 and rollNumber greater than 15.
 
-// let student =[
-//     {name:"Smith",rollNumber:31,marks:80},
-//     {name:"Jenny",rollNumber:15,marks:69},
-//     {name:"John",rollNumber:16,marks:35},
-//     {name:"Tiger",rollNumber:7,marks:55}
-//    ];
+let student = [
+    {name:"Dheeraj",rollNumber:31,marks:80},
+    {name:"Kuldeep",rollNumber:15,marks:69},
+    {name:"Hrishi",rollNumber:16,marks:65},
+    {name:"Rakesh",rollNumber:7,marks:55}
+   ];
 
 
+const results = student.filter(function(obj) {
+    return obj.marks > 60 && obj.rollNumber > 15
+}).map(function(obj) {
+    return obj.name;
+});
 
-// Solution here ->
+console.log(results);
