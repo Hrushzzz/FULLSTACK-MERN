53121e0845b6b61365274f61e745b3f542c3a4cf
SpiderMan <hrushikesh.tk@gmail.com>
2024-12-18 00:55:59 +0530

    dynamic imports

--- Ch03 - React/performance_rendering/src/App.js
+++ Ch03 - React/performance_rendering/src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+// import HomePage from "./Components/HomePage";
+// import ContactPage from "./Components/ContactPage";
+// import AboutPage from "./Components/AboutPage";
+// import Navbar from "./Components/Navbar";
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [HomePage, setHomePage] = useState(null);
+  const [AboutPage, setAboutPage] = useState(null);
+  const [ContactPage, setContactPage] = useState(null);
+
+  //preloading the homepage component
+  useEffect(() => {
+    //this is the async way of loading our component --> import("./Components/HomePage")
+    import("./Components/HomePage").then((module) =>
+      setHomePage(() => module.default)
+    );
+  }, []);
+
+  const loadHomePage = () => {
+    import("./Components/HomePage").then((module) =>
+      setHomePage(() => module.default)
+    );
+  };
+
+  const loadAboutPage = () => {
+    import("./Components/AboutPage").then((module) =>
+      setAboutPage(() => module.default)
+    );
+  };
+
+  const loadContactPage = () => {
+    import("./Components/ContactPage").then((module) =>
+      setContactPage(() => module.default)
+    );
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+    <BrowserRouter>
+      <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" onClick={loadHomePage}>Home</Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={loadAboutPage}>About</Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={loadContactPage}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={HomePage ? <HomePage /> : <div>Loading...</div>}></Route>
+        <Route path="/contact" element={ContactPage ? <ContactPage /> : <div>Loading...</div>}></Route>
+        <Route path="/about" element={AboutPage ? <AboutPage />: <div>Loading...</div>} ></Route>
+      </Routes>
+      </div>
+    </BrowserRouter>
+    </>
+    
   );
 }
 
