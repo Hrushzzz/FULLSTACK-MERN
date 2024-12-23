47e673693b9a7b4ab63d568f978394ed1fe08ae8
SpiderMan <hrushikesh.tk@gmail.com>
2024-12-23 13:36:59 +0530

    customHook - useVisibility

--- Ch03 - React/06 - Machine Coding/useVisibility/src/App.jsx
+++ Ch03 - React/06 - Machine Coding/useVisibility/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import Modals from './Components/Modals';
+import useVisibility from './useVisibility';
 
 function App() {
-
+  const {isVisible, toggle, show, hide} = useVisibility(false);
 
   return (
     <>
       <div>
+        <div>
+          <h1>Custom Hook Example</h1>
+          <button onClick={show}>Show</button>
+          <button onClick={toggle}>Toggle</button>
+          <Modals isVisible={isVisible} hide={hide}></Modals>
+        </div>
 
       </div>
     </>
