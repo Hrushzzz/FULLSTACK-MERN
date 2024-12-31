a5ca9b1f73ed42058f73e66ef9ebb2a72d6de651
SpiderMan <hrushikesh.tk@gmail.com>
2024-12-31 09:48:44 +0530

    mongoDB connection & CRUD

--- Ch04 - MERN/03 - Intro to MongoDB/blog/controllers/blog.controller.js
+++ Ch04 - MERN/03 - Intro to MongoDB/blog/controllers/blog.controller.js
@@ -1,64 +1,54 @@
 import fs from "fs";
+import Blog from "../model/blog.model.js";
 
-export const getAllBlog = (req, res) => {
+export const getAllBlog = async (req, res) => {
     try {
-        const blogData = fs.readFileSync("mocks/blog.mock.json");
-        res.status(200).send(JSON.parse(blogData));
+        const allBlogsData = await Blog.find({});   // ".find" ->  method in MongoDB
+        res.status(200).send(allBlogsData);
     } catch(e) {
         res.status(500).send(e.message);
     }
 }
 
-export const getBlogById = (req, res) => {
-    try {
-        const blogId = req.params.blogId;   //blogId is a dynamic param we passed in "blog.route.js",
-        // we can access it using ".param"
-        const blogData = fs.readFileSync("mocks/blog.mock.json");
-        const blogDataById = JSON.parse(blogData).find(blog => blog.id == blogId);
-        res.status(200).send(blogDataById);
-    } catch(e) {
-        res.status(500).send(e.message);
-    }
-}
-
-export const deleteBlogById= (req, res) => {
+export const getBlogById = async (req, res) => {
     try {
         const blogId = req.params.blogId;   //blogId is a dynamic param we passed in "blog.route.js",
         // we can access it using ".params"
-        const blogData = JSON.parse(fs.readFileSync("mocks/blog.mock.json"));
-        const blogIndexDataById = blogData.findIndex(blog => blog.id == blogId);
-        blogData.splice(blogIndexDataById, 1);
-        fs.writeFileSync("mocks/blog.mock.json", JSON.stringify(blogData));  // setting the data back, after deletion
-        res.status(200).send("Blog deleted");
+        const blogData = await Blog.findById(blogId);   // "findByID" -> method in MongoDB
+        res.status(200).send(blogData);
     } catch(e) {
         res.status(500).send(e.message);
     }
 }
 
-export const createBlog = (req, res) => {
+export const deleteBlogById = async (req, res) => {
+    try {
+        const blogId = req.params.blogId;
+        const data = await Blog.findByIdAndDelete(blogId);   // "findByIdAndDelete" -> method in MongoDB
+        res.status(200).send(data);
+    } catch(e) {
+        res.status(500).send(e.message);
+    }
+}
+
+export const createBlog = async (req, res) => {
     try {
         const newBlogData = req.body;  // we'll get the new data in the request body
-        console.log(newBlogData);
-        let blogData = JSON.parse(fs.readFileSync("mocks/blog.mock.json"));  //reading the data
-        blogData = [...blogData, newBlogData];   //updating the data
-        fs.writeFileSync("mocks/blog.mock.json", JSON.stringify(blogData));  //setting the data back, after adding a blog
-        res.status(200).send("Blog created successfully..!");  //sending success status
+        const data = await Blog.create(newBlogData);   //"Blog" is our model instance name 
+        //".create" -> method in MongoDB
+        res.status(200).send(data);
     } catch(e) {
         res.status(500).send(e.message);
     }
 }
 
-export const updateBlogById = (req, res) => {
+export const updateBlogById = async (req, res) => {
     try {
         const newBlogData = req.body;
         const blogId = req.params.blogId;
-        let blogData = JSON.parse(fs.readFileSync("mocks/blog.mock.json"));
-        const blogIndexDataById = blogData.findIndex(blog => blog.id == blogId);
-        blogData[blogIndexDataById] = {...blogData[blogIndexDataById] , ...newBlogData};
-        // ...newBlogData  ==> only the modified data will be updated,
-        //if we just pass "newBlogData" ==> entire Array will be overridden, which will lead to loss of unchanged data.
-        fs.writeFileSync("mocks/blog.mock.json", JSON.stringify(blogData));
-        res.status(200).send("Blog updated successfully..!");
+        const data = await Blog.updateOne({ _id: blogId }, {$set: newBlogData});
+        //".updateOne" -> method in MongoDB
+        res.status(200).send(data);
     } catch(e) {
         res.status(500).send(e.message);
     }
--- Ch04 - MERN/03 - Intro to MongoDB/blog/database/mongoDb.js
+++ Ch04 - MERN/03 - Intro to MongoDB/blog/database/mongoDb.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose"
 const connectToDB = async() => {
     try {
         const {connection} = await mongoose.connect(
-            "mongodb+srv://blogDB:Hrishi%401748@cluster0.hw4bw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb+srv://blogDB:Hrishi%401748@cluster0.hw4bw.mongodb.net/blogDB?retryWrites=true&w=majority&appName=Cluster0"
+            // We passed our database name here(before "?" mark)
         )
         if(connection) {
             console.log(`Connected to database ::: ${connection.host}`);
