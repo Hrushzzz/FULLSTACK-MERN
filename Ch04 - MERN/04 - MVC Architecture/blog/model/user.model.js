2cdf99cb53a55d45a57c0e35b47ef8fa4965516b
SpiderMan <hrushikesh.tk@gmail.com>
2025-01-02 06:33:04 +0530

    pre & post hooks

--- Ch04 - MERN/04 - MVC Architecture/blog/model/user.model.js
+++ Ch04 - MERN/04 - MVC Architecture/blog/model/user.model.js
@@ -24,6 +24,37 @@ const userSchema = new Schema({
         }
 });
 
+
+// Middleware :::
+// Middleware functions (also known as pre and post hooks) enable developers to execute custom logic 
+// before or after certain methods or actions are performed on Mongoose models or documents.
+
+userSchema.pre("save", function(next) {
+    const user = this;
+    console.log("Pre Hook");
+    console.log(user);
+    user.password = user.password + "-encrypted";
+    // "next()" function is very important as use it to pass down the control.
+    // Every middleware hook should have a "next()" function.
+    next();
+});
+
+userSchema.post("save", function(next) {
+    const user = this;
+    console.log("Post Hook");
+    console.log(user);
+    next();
+});
+
 const User = model('user', userSchema);
 
 export default User;
+
+
+// PRE hook ::: Executes before the operation. Use next() to proceed or throw an error to halt.
+// POST hook ::: Executes after the operation has completed.
+// Middleware functions can be synchronous or asynchronous.
+// Middleware is registered on the schema, not the model.
+
+// Mongoose middleware makes your application more maintainable and expressive by allowing 
+// repetitive tasks to be handled automatically and consistently across your application.
