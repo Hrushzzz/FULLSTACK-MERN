e31b85a67cc9878b602267e4ddc3e3a04ce587e0
SpiderMan <hrushikesh.tk@gmail.com>
2025-01-09 21:59:25 +0530

    booking controller

--- Ch05 - Projects/Pr05_CineMate/CineMate/controllers/booking.controller.js
+++ Ch05 - Projects/Pr05_CineMate/CineMate/controllers/booking.controller.js
@@ -1,8 +1,8 @@
 import Booking from "../model/booking.model.js";
 
-export const getPaymentClientSecret = async (req, res) => {
+export const makePayment = async (req, res) => {
     try {
-
+        // TODO : Make payment to Stripe and return transactionId
     } catch(e) {
         res.status(500).send({
             success: false,
@@ -13,7 +13,16 @@ export const getPaymentClientSecret = async (req, res) => {
 
 export const createBooking = async (req, res) => {
     try {
+        // UserId --> req.user (jwt token)
+        // Transaction Id --> req.transactionId (get transaction details from /make-payment)
+        // Seats --> req.seats (verify if selected seats are really available, update bookedSeats in Show collection)
+        // showId --> req.showId
 
+        const bookingDetails = req.body;
+        const booking = new Booking(bookingDetails);
+        booking.user = req.user.id;
+        await booking.save();
+        // TODO : verify if selected seats are really available, update bookedSeats in Show collection
     } catch(e) {
         res.status(500).send({
             success: false,
@@ -24,7 +33,27 @@ export const createBooking = async (req, res) => {
 
 export const getBookingDetail = async (req, res) => {
     try {
-
+        // We have to get user details, show details, movie details and theatre details
+        const bookingDetail = await Booking.find()
+        .populate("user")
+        .populate({   // getting "movie" details which is in "show" model
+            path: "show",
+            model: "shows",
+            populate: {
+                path: "movie",
+                model: "movies"
+            },
+        })
+        .populate({   // getting "theatre" details which is in "show" model
+            path: "show",
+            model: "shows",
+            populate: {
+                path: "theatre",
+                model: "theatres"
+            },
+        });
+        // this is called as "nested Populate".
+        res.send(bookingDetail);
     } catch(e) {
         res.status(500).send({
             success: false,
--- Ch05 - Projects/Pr05_CineMate/CineMate/routes/booking.route.js
+++ Ch05 - Projects/Pr05_CineMate/CineMate/routes/booking.route.js
@@ -1,12 +1,12 @@
 import express from "express";
 import AuthMiddleware from "../middleware/auth.middleware.js";
-import { createBooking, getBookingDetail, getPaymentClientSecret } from '../controllers/booking.controller.js';
+import { createBooking, getBookingDetail, makePayment } from '../controllers/booking.controller.js';
 
 const router = express.Router();
 
 // => Creating the routes :::
 
-router.post("/get-payment-secret", AuthMiddleware, getPaymentClientSecret);
+router.post("/make-payemnt", AuthMiddleware, makePayment);
 
 // create booking
 router.post("/confirm", AuthMiddleware, createBooking);
