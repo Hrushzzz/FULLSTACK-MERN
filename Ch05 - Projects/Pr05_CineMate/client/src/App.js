80dc5273d2b233694407a3c63f13890e375668c1
SpiderMan <hrushikesh.tk@gmail.com>
2025-01-15 08:15:16 +0530

    routes

--- Ch05 - Projects/Pr05_CineMate/client/src/App.js
+++ Ch05 - Projects/Pr05_CineMate/client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import MovieList from './pages/MovieList';
+import TheatreList from './pages/TheatreList';
+import ShowsList from './pages/ShowList';
+import MovieDetail from './pages/MovieDetail';
+import Theatres from './pages/Theatres';
+import ShowPage from './pages/ShowDetail';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/movie/:movieId/theatres/:theatreId/shows/:showId" element={<ShowPage />} />
+          <Route path="/movie/:movieId" element={<MovieDetail />} />
+          <Route path="/movie/:movieId/theatres" element={<Theatres />} />
+
+          <Route path="/admin/movies" element={<MovieList />} />
+          <Route path="/admin/theatres" element={<TheatreList />} />
+          <Route path="/admin/theatres/:theatreId/shows" element={<ShowsList />} />
+
+          <Route path="/owner/theatres" element={<TheatreList />} />
+          <Route path="/owner/theatres/:theatreId/shows" element={<ShowsList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
